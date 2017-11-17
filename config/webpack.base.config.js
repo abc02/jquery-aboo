87e6c113972fe8b3eedab3a03e085fad808a2a78
abc02 <abcgg02@gmail.com>
2017-11-17 17:01:46 +0800

    新增 download

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -41,11 +41,26 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: [
+                    'style-loader',
+                    {
+                      loader: 'css-loader',
+                      options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
+                  ]
             },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']
+                use: [
+                    'style-loader',
+                    {
+                      loader: 'css-loader',
+                      options: { importLoaders: 1 }
+                    },
+                    'postcss-loader',
+                    'sass-loader'
+                  ]
             },
             {
                 test: /\.(png|jpe?g|)$/,
