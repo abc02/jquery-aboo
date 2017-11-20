a84a7883b1af633f4523d8ea9869ca832a19f627
abc02 <abcgg02@gmail.com>
2017-11-20 13:30:57 +0800

    首页少个 闭合div标签

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -10,22 +10,6 @@ import '../../../node_modules/fonts.css/fonts.css'
 import './index.scss'
 
 
-// runSection = new FullPage({
-
-//     id: 'pageContain',                            // id of contain
-//     slideTime: 1000,                               // time of slide
-//     effect: {                                     // slide effect
-//         transform: {
-//             translate: 'X',					   // 'X'|'Y'|'XY'|'none'
-//             scale: [0, 1],					   // [scalefrom, scaleto]
-//             rotate: [1, 0]					   // [rotatefrom, rotateto]
-//         },
-//         opacity: [0, 1]                     // [opacityfrom, opacityto]
-//     },
-//     mode: 'touch,nav:navBar',               // mode of fullpage
-//     easing: [0, .93, .39, .98],             // easing('ease','ease-in','ease-in-out' or use cubic-bezier like [.33, 1.81, 1, 1] )
-
-// });
 $(function () {
     let runPage,
         runSection;
